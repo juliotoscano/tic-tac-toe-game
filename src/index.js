2741617bd0f47378c20f4e7ef63221f9e08dadce
juliotoscano <toscanodeveloper@gmail.com>
2021-11-20 15:08:38 +0000

    release tic tac toe 1.0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Game from '../src/application/reactcomponent/Game';
 import './index.css';
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
